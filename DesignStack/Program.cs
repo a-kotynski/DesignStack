0641a2fc878ebfa7ba9874d4f49d91fdc12232ad
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-08-16 14:30:09 +0200

    Add project files.

---
+++ DesignStack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignStack
+{
+    public class Stack
+    {
+        public List<object> StackList { get; set; }
+        public void Push(object obj)
+        {
+            try
+            {
+                StackList.Add(obj);
+            }
+            catch (InvalidOperationException)
+            {
+
+                throw;
+            }
+        }
+        public object Pop()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (InvalidOperationException)
+            {
+
+                throw;
+            }
+        }
+        public void Clear()
+        {
+            StackList.Clear();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
